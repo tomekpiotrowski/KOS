482409490c3fb03bb08c50c5264115010fbf0eae
Tomek Piotrowski <tomek.piotrowski@atteo.com>
2016-04-12 17:43:16 +0200

    Revert "Add deprecation messages to old rename/delete/copy syntax"
    This reverts commit 9ee3289f83873195fe0dbf40b45de4cd392d00fa.

--- src/kOS/Binding/FlightStats.cs
+++ src/kOS/Binding/FlightStats.cs
@@ -17,7 +17,7 @@ namespace kOS.Binding
             shared.BindingMgr.AddGetter("COMMRANGE", () => int.MaxValue);
             shared.BindingMgr.AddGetter("ENCOUNTER", () => VesselUtils.TryGetEncounter(shared.Vessel,shared));
             shared.BindingMgr.AddGetter("ETA", () => new VesselEta(shared));
-            shared.BindingMgr.AddGetter("INCOMMRANGE", () => { throw new KOSDeprecationException("0.17.0", "INCOMMRANGE", "ADDONS:RT:HASCONNECTION(VESSEL)"); });
+            shared.BindingMgr.AddGetter("INCOMMRANGE", () => { throw new KOSDeprecationException("0.17.0", "INCOMMRANGE", "ADDONS:RT:HASCONNECTION(VESSEL)", @"http://ksp-kos.github.io/KOS_DOC/addons/RemoteTech.html"); });
             shared.BindingMgr.AddGetter("MISSIONTIME", () => shared.Vessel.missionTime);
             shared.BindingMgr.AddGetter(new [] { "OBT" , "ORBIT"}, () => new OrbitInfo(shared.Vessel.orbit,shared));
             shared.BindingMgr.AddGetter("TIME", () => new TimeSpan(Planetarium.GetUniversalTime()));
